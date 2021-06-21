6a78a47bd20dff12921ed1edc9a34823510715b3
Abhi-4793 <abhinavkrishnapy2@gmail.com>
2021-06-21 23:32:38 +0530

    completed

---
+++ venv/weather.py
@@ -0,0 +1,35 @@
+import requests
+#import os
+from datetime import datetime
+
+api_key = 'e9e795ab84daca0ac5ee506a6975bb2c'
+location = input("Enter the city name: ")
+
+complete_api_link = "https://api.openweathermap.org/data/2.5/weather?q="+location+"&appid="+api_key
+api_link = requests.get(complete_api_link)
+api_data = api_link.json()
+
+
+temp_city = ((api_data['main']['temp']) - 273.15)
+weather_desc = api_data['weather'][0]['description']
+hmdt = api_data['main']['humidity']
+wind_spd = api_data['wind']['speed']
+date_time = datetime.now().strftime("%d %b %Y | %I:%M:%S %p")
+
+print ("-------------------------------------------------------------")
+print ("Weather Stats for - {}  || {}".format(location.upper(), date_time))
+print ("-------------------------------------------------------------")
+
+print ("Current temperature is: {:.2f} deg C".format(temp_city))
+print ("Current weather desc  :",weather_desc)
+print ("Current Humidity      :",hmdt, '%')
+print ("Current wind speed    :",wind_spd ,'kmph')
+str1 = str(date_time)
+str2 = str(temp_city)
+str3 = str(hmdt)
+str4 = str(weather_desc)
+str5 = str(wind_spd)
+L = [str1, "\t", str2, "\t", str3,"\t", str4,"\t", str5]
+with open('weatherfile.txt','a') as file:
+ file.writelines(L)
+ file.write("\n")
